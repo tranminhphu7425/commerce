6d67b3e03d7cc15c94802ab492a20e3c83df46f4
tranminhphu7425 <phub2306573@student.ctu.edu.vn>
2026-05-13 23:15:04 +0700

    Fix: properly remove distDir and update build script

---
+++ docs/_next/static/m2kGGi3UCV6v7GU7XODTf/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fproduct\u002F[handle]","\u002Fsearch\u002F[collection]","\u002Fsearch\u002F[collection]\u002Fopengraph-image"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
